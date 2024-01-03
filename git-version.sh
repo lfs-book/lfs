e43d96fa2fba387bfa1a4c3934950002e56412b5
Xi Ruoyao <xry111@xry111.site>
2024-01-03 13:39:21 +0800

    mips64el: start the branch

--- git-version.sh
+++ git-version.sh
@@ -53,8 +53,8 @@ full_date="$month $day$suffix, $year"
 
 sha="$(git describe --abbrev=1)"
 rev=$(echo "$sha" | sed 's/-g[^-]*$//')
-version="$rev"
-versiond="$rev-systemd"
+version="mips64el-$rev"
+versiond="mips64el-$rev-systemd"
 
 if [ "$(git diff HEAD | wc -l)" != "0" ]; then
     version="$version-wip"
