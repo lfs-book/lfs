964ea6942926d309e9cdf048222ae2508d2ad2fc
Zeckmathederg <zeckma.tech@gmail.com>
2026-03-15 14:15:24 -0600

    git-version-ML: Fix version fetching.

--- git-version.sh
+++ git-version.sh
@@ -51,7 +51,7 @@ esac
 
 full_date="$month $day$suffix, $year"
 
-sha="$(git describe --abbrev=1)"
+sha="$(git describe --tags --abbrev=1)"
 rev=$(echo "$sha" | sed 's/-g[^-]*$//')
 version="$rev"
 versiond="$rev-systemd"
